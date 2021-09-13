b6c605a7974d795e08173741b26e5fce6897574e
Łukasz Piątkowski <piontec@gmail.com>
2021-09-13 15:58:17 +0200

    validation done

--- app_catalog_cleanup_tool/__main__.py
+++ app_catalog_cleanup_tool/__main__.py
@@ -1,8 +1,14 @@
 """Main module."""
 import asyncio
 import logging
+import os
+import re
+from dataclasses import dataclass
+from datetime import datetime, timezone
+from typing import Pattern, Optional
 
 import configargparse
+from tempora import parse_timedelta
 
 VER = "v0.0.0-dev"
 APP_NAME = "app_catalog_cleanup_tool"
@@ -62,7 +68,7 @@ def configure() -> configargparse.Namespace:
     exc_group.add_argument(
         "-b",
         "--delete-before",
-        help="A time delta (like '-3 days' or '-4 weeks') to keep catalog entries. Older entries matching also "
+        help="A time delta (like '3 days' or '4 weeks') to keep catalog entries. Older entries matching also "
         "'--arg-regexp' will be removed.",
     )
     config_parser.add_argument(
@@ -73,17 +79,60 @@ def configure() -> configargparse.Namespace:
         "--version", action="version", version=f"{APP_NAME} {get_version()}"
     )
     config = config_parser.parse_args()
+    if config.debug:
+        logger.setLevel(logging.DEBUG)
     return config
 
 
-def validate(config: configargparse.Namespace) -> None:
-    pass
+@dataclass
+class ValidatedConfig:
+    app_regexp: Pattern[str]
+    keep_from: datetime
+
+
+def validate(config: configargparse.Namespace) -> Optional[ValidatedConfig]:
+    try:
+        # check if catalog dir exists
+        if (
+            not config.path
+            or not os.path.isdir(config.path)
+            or not os.access(config.path, os.W_OK)
+        ):
+            raise ValueError(f"directory '{config.path}' not found")
+        # check if index.yaml is there
+        index_path = os.path.join(config.path, "index.yaml")
+        if (
+            not os.path.isfile(index_path)
+            or not os.access(index_path, os.W_OK)
+            or not os.access(index_path, os.R_OK)
+        ):
+            raise ValueError(
+                f"file 'index.yaml' doesn't exists in '{config.path}' or doesn't have RW permissions"
+            )
+        # parse regexp
+        try:
+            app_regexp = re.compile(config.app_regexp)
+        except re.error:
+            raise ValueError(
+                f"app name regexp '{config.app_regexp}' is not a valid regexp"
+            )
+        # parse since/before
+        keep_from = datetime.now(timezone.utc)
+        if config.keep_since:
+            keep_from = datetime.fromisoformat(config.keep_since)
+        if config.delete_before:
+            delta = parse_timedelta(config.delta_before)
+            keep_from -= delta
+        return ValidatedConfig(app_regexp, keep_from)
+
+    except ValueError as e:
+        logger.critical(f"Bad config: {e}.")
+        return None
 
 
 async def main() -> None:
     config = configure()
     validate(config)
-    await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
