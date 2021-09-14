d57fc06ba3f40b0b79b8141b4c2f5db188eb69ca
Łukasz Piątkowski <piontec@gmail.com>
2021-09-14 12:26:34 +0200

    YAML clenaup done

--- app_catalog_cleanup_tool/__main__.py
+++ app_catalog_cleanup_tool/__main__.py
@@ -3,12 +3,19 @@ import asyncio
 import logging
 import os
 import re
+import shutil
 import sys
+from copy import deepcopy
 from dataclasses import dataclass
 from datetime import datetime, timezone
-from typing import Pattern, Optional
+from typing import Pattern, Optional, Tuple, List
 
+import aiofiles
+import aiofiles.os
 import configargparse
+import pytz
+import yaml
+from dateutil import parser
 from tempora import parse_timedelta
 
 VER = "v0.0.0-dev"
@@ -93,6 +100,8 @@ def configure() -> configargparse.Namespace:
 class ValidatedConfig:
     app_regexp: Pattern[str]
     keep_from: datetime
+    path: str
+    index_yaml: str
 
 
 def validate(  # noqa :C901
@@ -126,18 +135,74 @@ def validate(  # noqa :C901
         # parse since/before
         keep_from = datetime.now(timezone.utc)
         if config.keep_since:
-            keep_from = datetime.fromisoformat(config.keep_since)
+            keep_from = parser.parse(config.keep_since)
+            if not keep_from.tzinfo:
+                keep_from = pytz.utc.localize(keep_from)
         if config.delete_before:
             try:
                 delta = parse_timedelta(config.delete_before)
             except Exception:
                 raise ValueError(f"can't parse time delta '{config.delete_before}'")
             keep_from -= delta
-        return ValidatedConfig(app_regexp, keep_from)
+        return ValidatedConfig(
+            app_regexp, keep_from, config.path, os.path.join(config.path, "index.yaml")
+        )
 
     except ValueError as e:
         logger.critical(f"Bad config: {e}.")
         return None
+
+
+async def clean_catalog(cfg: ValidatedConfig) -> None:
+    await clean_index(cfg)
+    # to_remove = await clean_index(cfg)
+
+
+def date_based_cleaner(cfg: ValidatedConfig, entries: list) -> Tuple[list, list]:
+    def keep(date: str) -> bool:
+        return parser.isoparse(date) >= cfg.keep_from
+
+    new_app_entries = [e for e in entries if keep(e["created"])]
+    entries_to_remove = [
+        f"{e['name']}-{e['version']}.tgz" for e in entries if not keep(e["created"])
+    ]
+
+    return new_app_entries, entries_to_remove
+
+
+async def clean_index(cfg: ValidatedConfig) -> List[str]:
+    async with aiofiles.open(cfg.index_yaml, mode="r") as f:
+        raw_yaml = await f.read()
+        index_yaml = yaml.safe_load(raw_yaml)
+    to_remove = []
+    entries = index_yaml["entries"]
+    logger.debug(f"Loaded {len(entries)} app entries from the catalog index.")
+    new_index_yaml = deepcopy(index_yaml)
+    new_entries = new_index_yaml["entries"]
+    for app_name in index_yaml["entries"].keys():
+        if not cfg.app_regexp.fullmatch(app_name):
+            logger.debug(
+                f"Skipping app '{app_name}' as it doesn't match the configured app name regexp."
+            )
+            continue
+        del new_entries[app_name]
+        before = len(entries[app_name])
+        new_app_entries, entries_to_remove = date_based_cleaner(cfg, entries[app_name])
+        if len(new_app_entries) > 0:
+            new_entries[app_name] = new_app_entries
+        if len(entries_to_remove) > 0:
+            to_remove.extend(entries_to_remove)
+        after = len(new_app_entries)
+        logger.info(
+            f"App '{app_name}' will be trimmed to {after} versions from the current {before}."
+        )
+
+    # save backup and the new file
+    shutil.move(cfg.index_yaml, cfg.index_yaml + ".back")
+    async with aiofiles.open(cfg.index_yaml, mode="w") as f:
+        await f.write(yaml.dump(new_index_yaml, default_flow_style=False))
+
+    return to_remove
 
 
 async def main() -> None:
@@ -146,6 +211,7 @@ async def main() -> None:
     if cfg is None:
         sys.exit(1)
     logger.info(f"Trying to remove all matching app entries before {cfg.keep_from}.")
+    await clean_catalog(cfg)
 
 
 if __name__ == "__main__":
