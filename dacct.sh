e3c41da23577c878704df9c97e969747a89b7b0b
Łukasz Piątkowski <piontec@gmail.com>
2021-09-17 09:50:45 +0200

    fix: wrong image name in dacct.sh

--- dacct.sh
+++ dacct.sh
@@ -2,9 +2,8 @@
 
 DACCT_TAG=${DACCT_TAG:-"latest"}
 
-  # "quay.io/giantswarm/app-build-suite:${DACCT_TAG}" "$@"
 docker run -it --rm \
   -e USE_UID="$(id -u "${USER}")" \
   -e USE_GID="$(id -g "${USER}")" \
   -v "$(pwd)":/acct/workdir/ \
-  "acct:${DACCT_TAG}" "$@"
+  "quay.io/giantswarm/app-catalog-cleanup-tool:${DACCT_TAG}" "$@"
