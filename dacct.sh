889d7d42e3b011a5809d213e692caa9aae01165c
Marian Steinbach <marian@giantswarm.io>
2024-02-01 09:43:59 +0100

    Replace quay.io with gsoci.azurecr.io

--- dacct.sh
+++ dacct.sh
@@ -6,4 +6,4 @@ docker run -it --rm \
   -e USE_UID="$(id -u "${USER}")" \
   -e USE_GID="$(id -g "${USER}")" \
   -v "$(pwd)":/acct/workdir/ \
-  "quay.io/giantswarm/app-catalog-cleanup-tool:${DACCT_TAG}" "$@"
+  "gsoci.azurecr.io/giantswarm/app-catalog-cleanup-tool:${DACCT_TAG}" "$@"
