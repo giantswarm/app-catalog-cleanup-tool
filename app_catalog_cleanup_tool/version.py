65fecb339fc122f8f30b5aafbaded1deca38a769
Łukasz Piątkowski <piontec@gmail.com>
2024-02-27 13:49:19 +0100

    Post-release version set for 0.2.6

--- app_catalog_cleanup_tool/version.py
+++ app_catalog_cleanup_tool/version.py
@@ -1,1 +1,1 @@
-build_ver = "0.2.6"
+build_ver = "0.2.6-dev"
--- dacct.sh
+++ dacct.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DACCT_TAG=${DACCT_TAG:-"0.2.6"}
+DACCT_TAG=${DACCT_TAG:-"latest"}
 
 docker run -it --rm \
 	-e USE_UID="$(id -u "${USER}")" \
